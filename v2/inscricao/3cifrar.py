5cf47e806931a6660b8b772b008790e01feb6aa2
Vinicius Dacio <51085313+ViniciusDacio@users.noreply.github.com>
2023-09-14 11:33:48 -0300

    Add files via upload
    Nova versão

---
+++ v2/inscricao/3cifrar.py
@@ -0,0 +1,21 @@
+import validacao.util as util
+import tenseal as ts
+
+img_biometria = "capturas/dacio.png"
+nome_usuario = "dacio"
+
+representacao_facial = util.extrair_caracteristicas(img_biometria, "inscricao/embedding/"+nome_usuario+".txt")
+
+def cifra(img1_embedding, chave):
+    context = ts.context_from(util.read_data(chave))
+    #criptografar a embeddings da pessoa 1
+    enc_v1 = ts.ckks_vector(context, img1_embedding)
+    #armazenar a embeddings criptografada homomórfica da pessoa 1
+    enc_v1_proto = enc_v1.serialize()
+
+    return enc_v1_proto
+
+img_cifrada = cifra(representacao_facial, "chaves/secret.txt")
+
+#armazenar a embeddings criptografada homomórfica da pessoa 
+util.write_data("database/"+nome_usuario+"_cifrado.txt", img_cifrada)
